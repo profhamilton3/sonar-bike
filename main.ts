69f3237414fa399a46871a49b86bdf421c32c7d4
T. Hamilton <profhamilton3@gmail.com>
2024-11-13 21:37:50 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function () {
+    if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_inch, DigitalPin.P8) < 3) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.UntilDone)
+    }
+})
