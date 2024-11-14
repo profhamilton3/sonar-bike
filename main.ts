f8634bf618133afed13567ddfa98c11e878a39cc
T. Hamilton <profhamilton3@gmail.com>
2024-11-13 21:38:26 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+basic.showIcon(IconNames.SmallHeart)
 basic.forever(function () {
     if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_inch, DigitalPin.P8) < 3) {
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.UntilDone)
